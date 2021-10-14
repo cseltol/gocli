20f40144cb47cd88a4065e2e9a5c5326708b1219
cseltol <xeltolgit@inbox.ru>
2021-10-14 14:20:44 +0300

    Initial Commit

---
+++ cmd/mcli/cli.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Web Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMES, MX records and Name Servers!"
+
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "host",
+			Value: "example.org",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks up the Name Servers for a Particular host",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a particular host",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupCNAME(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up the MX records for a particular host",
+			Flags: flags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gocli
+
+go 1.13
+
+require github.com/urfave/cli v1.22.5 // indirect
